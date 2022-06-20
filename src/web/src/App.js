39cebe55bdb8d7d635a4db22d6d1d0a8db964d43
TueeNguyen <tnguyen187@myseneca.ca>
2022-06-20 00:54:42 -0400

    Add a little demo to user token and isAuthenticated func in backend works

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -3,12 +3,25 @@ import Router from './components/router/Router';
 import { makeStyles } from '@mui/styles';
 import NavBar from './components/navBar/NavBar';
 import UtilBar from './components/utilBar/UtilBar';
-
+import { SWContext } from './contexts/SWContext';
+import { useEffect } from 'react';
+import { auth } from './firebase/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const { user } = await signInWithEmailAndPassword(auth, '1234@gmail.com', 'tuechinhlatue1');
+        console.log(user.accessToken);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
   return (
-    <div>
+    <SWContext.Provider>
       <Router />
-    </div>
+    </SWContext.Provider>
   );
 };
 
