62226870508dc8e9771e07e6301c02e610933a54
TueeNguyen <tnguyen187@myseneca.ca>
2022-06-25 12:39:26 -0400

    Add some fixes to join functionality: Front-end: - Add expense settings sidebar displaying id and password - Successful join redirects to expense
    Back-end:
    - Save expense password as string for now

--- src/api/src/controllers/password.ts
+++ src/api/src/controllers/password.ts
@@ -1,31 +1,31 @@
-import bcrypt from 'bcryptjs';
+// import bcrypt from 'bcryptjs';
 
-const encryptPassword = async (password: string): Promise<string> =>
-  new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      }
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        }
-        resolve(hash);
-      });
-    });
-  });
+// const encryptPassword = async (password: string): Promise<string> =>
+//   new Promise((resolve, reject) => {
+//     bcrypt.genSalt(10, (err, salt) => {
+//       if (err) {
+//         console.error(err);
+//         reject(err);
+//       }
+//       bcrypt.hash(password, salt, (err, hash) => {
+//         if (err) {
+//           console.error(err);
+//           reject(err);
+//         }
+//         resolve(hash);
+//       });
+//     });
+//   });
 
-const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> =>
-  new Promise((resolve, reject) => {
-    bcrypt.compare(password, hashedPassword, (err, isMatched) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      }
-      resolve(isMatched);
-    });
-  });
+// const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> =>
+//   new Promise((resolve, reject) => {
+//     bcrypt.compare(password, hashedPassword, (err, isMatched) => {
+//       if (err) {
+//         console.error(err);
+//         reject(err);
+//       }
+//       resolve(isMatched);
+//     });
+//   });
 
-export { encryptPassword, comparePassword };
+// export { encryptPassword, comparePassword };
