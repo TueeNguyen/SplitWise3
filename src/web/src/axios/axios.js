d0a8373f09356e9aae1cc3d8d6ea632b42ec477d
TueeNguyen <tnguyen187@myseneca.ca>
2022-07-04 18:34:04 -0400

    Changes to make images work on aws EB

--- src/web/src/axios/axios.js
+++ src/web/src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:6060/api'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost/api/'
 });
+console.log(process.env.REACT_APP_API_URL);
 
 export default axiosInstance;
