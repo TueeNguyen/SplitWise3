3e86eb23aab1d0f28c32dd3f8d5a8bb64882287c
Tue Nguyen <58532267+TueeNguyen@users.noreply.github.com>
2022-08-30 13:31:47 -0400

    Reworked database configs for dev environment (#78)
    - Add postinstall script to create configs.ts file
    - Make config.ts ignored by Git for developers to safely save their firebase credentials
    - Reworked database initialization files

--- src/api/src/firebase/firebase.ts
+++ src/api/src/firebase/firebase.ts
@@ -2,7 +2,30 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { firebaseCredentials } from './configs';
 
-const app = initializeApp(firebaseCredentials);
+let appCredentials = {};
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'development') {
+  appCredentials = firebaseCredentials;
+}
+
+if (process.env.NODE_ENV === 'production') {
+  appCredentials = {
+    type: process.env.ADMIN_TYPE,
+    project_id: process.env.ADMIN_PROJECT_ID,
+    private_key_id: process.env.ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.ADMIN_CLIENT_EMAIL,
+    client_id: process.env.ADMIN_CLIENT_ID,
+    auth_uri: process.env.ADMIN_AUTH_URI,
+    token_uri: process.env.ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.ADMIN_CLIENT_X509_CERT_URL
+  };
+}
+
+const app = initializeApp(appCredentials);
 
 const auth = getAuth(app);
 
